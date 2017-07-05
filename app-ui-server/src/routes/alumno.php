0d7f8ca9cfde949099f8208349c240a4def027c9
V619324 <luis.bazanchavez@one.verizon.com>
2017-07-04 20:40:45 -0500

    PHP Cambios

--- app-ui-server/src/routes/alumno.php
+++ app-ui-server/src/routes/alumno.php
@@ -2,11 +2,29 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-$app = new \Slim\App;
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host']   = "localhost";
+$config['db']['user']   = "serprosa_admin";
+$config['db']['pass']   = "serprosac$2017";
+$config['db']['dbname'] = "serprosa_capacitacion";
+
+$app = new \Slim\App(["settings" => $config]);
 
 $app->options('/{routes:.+}', function ($request, $response, $args) {
     return $response;
 });
+
+$container = $app->getContainer();
+
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
@@ -17,6 +35,7 @@ $app->add(function ($req, $res, $next) {
 
 //Get All Alumnos
 $app->get('/alumnos', function(Request $request, Response $response, $args){
+    $this->logger->addInfo("get All Alumnos");
     $sql = "SELECT * FROM alumnos";
     try{
         // Get DB Object
@@ -29,5 +48,6 @@ $app->get('/alumnos', function(Request $request, Response $response, $args){
         echo json_encode($customers);
     } catch(PDOException $e){
         echo '{"error": {"text": '.$e->getMessage().'}';
+        $this->logger->addError($e->getMessage());
     }
 });
--- app-ui-server/vendor/composer/autoload_static.php
+++ app-ui-server/vendor/composer/autoload_static.php
@@ -17,8 +17,13 @@ class ComposerStaticInit57efcbd1b87bd25b0cf3562f88c8b1b5
         ),
         'P' => 
         array (
+            'Psr\\Log\\' => 8,
             'Psr\\Http\\Message\\' => 17,
             'Psr\\Container\\' => 14,
+        ),
+        'M' => 
+        array (
+            'Monolog\\' => 8,
         ),
         'I' => 
         array (
@@ -35,6 +40,10 @@ class ComposerStaticInit57efcbd1b87bd25b0cf3562f88c8b1b5
         array (
             0 => __DIR__ . '/..' . '/slim/slim/Slim',
         ),
+        'Psr\\Log\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
+        ),
         'Psr\\Http\\Message\\' => 
         array (
             0 => __DIR__ . '/..' . '/psr/http-message/src',
@@ -42,6 +51,10 @@ class ComposerStaticInit57efcbd1b87bd25b0cf3562f88c8b1b5
         'Psr\\Container\\' => 
         array (
             0 => __DIR__ . '/..' . '/psr/container/src',
+        ),
+        'Monolog\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/monolog/monolog/src/Monolog',
         ),
         'Interop\\Container\\' => 
         array (
